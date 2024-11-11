66529af1903691a900281bf7bf62493c5a349dd0
Mercy <sheroudensi@gmail.com>
2024-11-11 20:31:42 +0100

    My Portfolio

--- index.js
+++ index.js
@@ -17,4 +17,41 @@ function switchTo(sectionClass){
         element.scrollIntoView({behavior:'smooth'});
     }
 }
+let isMuted = true;
+function toggleMute(){
+    isMuted = !isMuted;
 
+
+    const playMusic = document.querySelector('.hover-text-music');
+    const pauseMusic = document.querySelector('.hoverr-text-music')
+    const musicOnIcon = document.querySelector('.music-on')
+    const musicOffIcon = document.querySelector('.music-off')
+    const toggleOnIcon = document.querySelector('#toggle-on');
+    const toggleOffIcon = document.querySelector('#toggle-off');
+    const audio = document.getElementById('myAudio');
+    if (isMuted){
+        toggleOnIcon.style.display = 'none';
+        toggleOffIcon.style.display = 'inline'
+        musicOffIcon.style.display = 'block';
+        musicOnIcon.style.display = 'none';
+        playMusic.style.display = 'block';
+        pauseMusic.style.display = 'none';
+        audio.pause()
+        setTimeout(() =>{
+            playMusic.style.display = 'none';
+        }, 1000);
+        }
+      else {
+        toggleOnIcon.style.display = 'inline';
+        toggleOffIcon.style.display = 'none';
+        audio.play()
+        musicOffIcon.style.display = 'none';
+        musicOnIcon.style.display = 'block';
+        playMusic.style.display = 'none';
+        pauseMusic.style.display = 'block';
+    }
+    setTimeout(() =>{
+        pauseMusic.style.display = 'none';
+    }, 1000);
+
+}
